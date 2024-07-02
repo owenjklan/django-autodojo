65282c39bc523ef1834af2f5feac3129f687215d
Owen Klan <owen.j.klan@gmail.com>
2024-07-02 18:22:12 +1000

    Add database config for Django

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,16 @@
+from pathlib import Path
+
 SECRET_KEY = "fake-key"
 INSTALLED_APPS = [
     "tests",
 ]
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
